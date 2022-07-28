95c8975358d64e3562b8ed58cb0dd902f94ef9c2
Isaac Anane Amankwaah <57358941+brasmith9@users.noreply.github.com>
2022-07-28 10:06:26 +0000

    debuging⚔️

--- index.js
+++ index.js
@@ -1,18 +1,24 @@
 import express from "express";
-import apiRoutes from "./routes/index.js"
+import morgan from "morgan";
+import helmet from "helmet";
+import bodyParser from "body-parser";
+import apiRoutes from "./routes/index.js";
 
 const app = express();
 const port = 8080;
 
 app.use(express.json());
+app.use(express.urlencoded());
+app.use(bodyParser.json());
+app.use(helmet());
+app.use(morgan("combined"));
 
-//Root Page
+// Root Page
 app.get("/", (req, res) => {
   res.send("Welcome to our page🚀");
 });
 
 app.use(apiRoutes);
-
 
 app.listen(port, () => {
   console.log(`App started at http://localhost:${port}`);
--- middlewares/index.js
+++ middlewares/index.js
@@ -4,7 +4,7 @@ import { users } from "../data.js";
 const checkIfUserExist = (req, res, next) => {
   const user = users.find((item) => item.id == req.params.userId);
   if (!user) {
-    res.status(404).send({
+    return res.status(404).send({
       message: "No user exist with the given ID",
       data: null,
     });
